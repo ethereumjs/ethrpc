5134dfc8bd8b424337276a08012637ef6ad2eab5
Jack Peterson <jack@tinybike.net>
2017-04-17 20:53:07 -0700

    convertResponseToReturnsType formatting

--- src/decode-response/convert-response-to-returns-type.js
+++ src/decode-response/convert-response-to-returns-type.js
@@ -6,32 +6,30 @@ var clone = require("clone");
 var convertResponseToReturnsType = function (returnsType, response) {
   var convertedResponse;
   if (!returnsType) return response;
-  if (response && response !== "0x") {
-    if (response.error) return response;
-    returnsType = returnsType.toLowerCase();
-    convertedResponse = clone(response);
-    if (returnsType && returnsType.slice(-2) === "[]") {
-      convertedResponse = abi.unroll_array(convertedResponse, returnsType);
-      if (returnsType === "hash[]") {
-        convertedResponse = abi.hex(convertedResponse);
-      }
-    } else if (returnsType === "string") {
-      convertedResponse = abi.raw_decode_hex(convertedResponse);
-    } else if (returnsType === "number") {
-      convertedResponse = abi.string(convertedResponse, true);
-    } else if (returnsType === "int") {
-      convertedResponse = abi.number(convertedResponse, true);
-    } else if (returnsType === "bignumber") {
-      convertedResponse = abi.bignum(convertedResponse, null, true);
-    } else if (returnsType === "unfix") {
-      convertedResponse = abi.unfix_signed(convertedResponse, "string");
-    } else if (returnsType === "null") {
-      convertedResponse = null;
-    } else if (returnsType === "address" || returnsType === "address[]") {
-      convertedResponse = abi.format_address(convertedResponse);
+  if (!response || response === "0x") return response;
+  if (response.error) return response;
+  returnsType = returnsType.toLowerCase();
+  convertedResponse = clone(response);
+  if (returnsType && returnsType.slice(-2) === "[]") {
+    convertedResponse = abi.unroll_array(convertedResponse, returnsType);
+    if (returnsType === "hash[]") {
+      convertedResponse = abi.hex(convertedResponse);
     }
-  } else {
-    convertedResponse = response;
+    return convertedResponse;
+  } else if (returnsType === "string") {
+    return abi.raw_decode_hex(convertedResponse);
+  } else if (returnsType === "number") {
+    return abi.string(convertedResponse, true);
+  } else if (returnsType === "int") {
+    return abi.number(convertedResponse, true);
+  } else if (returnsType === "bignumber") {
+    return abi.bignum(convertedResponse, null, true);
+  } else if (returnsType === "unfix") {
+    return abi.unfix_signed(convertedResponse, "string");
+  } else if (returnsType === "null") {
+    return null;
+  } else if (returnsType === "address" || returnsType === "address[]") {
+    return abi.format_address(convertedResponse);
   }
   return convertedResponse;
 };
