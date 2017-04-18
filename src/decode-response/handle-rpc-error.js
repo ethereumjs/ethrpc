4d2fbd10e92d1d66cd76b0be64e896a50ac54428
Jack Peterson <jack@tinybike.net>
2017-04-17 20:32:22 -0700

    Tweaked handleRPCError formatting

--- src/decode-response/handle-rpc-error.js
+++ src/decode-response/handle-rpc-error.js
@@ -5,26 +5,23 @@ var errors = require("../errors/codes");
 
 var handleRPCError = function (method, returns, response) {
   var i, len, responseNumber;
-  if (response) {
-    if (response.constructor === Array) {
-      for (i = 0, len = response.length; i < len; ++i) {
-        response[i] = handleRPCError(method, returns, response[i]);
-      }
-    } else if (response.name && response.message && response.stack) {
-      response.error = response.name;
-    } else if (!response.error) {
-      if (returns && returns.indexOf("[]") > -1 && response.length >= 194) {
-        response = "0x" + response.slice(130, 194);
-      }
-      if (errors[response]) {
-        response = { error: response, message: errors[response] };
-      } else if (returns !== "null" && returns !== "string" || (typeof response === "string" && response.slice(0, 2) === "0x")) {
-        responseNumber = abi.bignum(response, "string", true);
-        if (responseNumber) {
-          if (errors[method] && errors[method][responseNumber]) {
-            response = { error: responseNumber, message: errors[method][responseNumber] };
-          }
-        }
+  if (!response) return response;
+  if (Array.isArray(response)) {
+    for (i = 0, len = response.length; i < len; ++i) {
+      response[i] = handleRPCError(method, returns, response[i]);
+    }
+  } else if (response.name && response.message && response.stack) {
+    response.error = response.name;
+  } else if (!response.error) {
+    if (returns && returns.indexOf("[]") > -1 && response.length >= 194) {
+      response = "0x" + response.slice(130, 194);
+    }
+    if (errors[response]) {
+      response = { error: response, message: errors[response] };
+    } else if (returns !== "null" && returns !== "string" || (typeof response === "string" && response.slice(0, 2) === "0x")) {
+      responseNumber = abi.bignum(response, "string", true);
+      if (responseNumber && errors[method] && errors[method][responseNumber]) {
+        response = { error: responseNumber, message: errors[method][responseNumber] };
       }
     }
   }
