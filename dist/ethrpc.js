e68ad94f0d09ab6a4626ca4646e49d89abff077f
Jack Peterson <jack@tinybike.net>
2017-06-06 14:10:56 -0700

    Temporarily disabled web3 transport

--- dist/ethrpc.js
+++ dist/ethrpc.js
@@ -42597,7 +42597,7 @@ function Transporter(configuration, messageHandler, syncOnly, debugLogging, call
     // only use web3 transport if we're on mainnet (1) or public testnet (3)
     var networkID = parseInt(configuration.networkID, 10);
     var isMainnetOrPublicTestnet = !isNaN(networkID) && (networkID === 1 || networkID === 3);
-    resultAggregator.web3Transports[0] = (error !== null || !isMainnetOrPublicTestnet) ? null : web3Transport;
+    resultAggregator.web3Transports[0] = null; //(error !== null || !isMainnetOrPublicTestnet) ? null : web3Transport;
     checkIfComplete(this, resultAggregator, callback);
   }.bind(this));
   configuration.ipcAddresses.forEach(function (ipcAddress, index) {
