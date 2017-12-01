69d204af65096ca6685ca03e974a4318b71bafe7
Scott Bigelow <epheph@gmail.com>
2017-11-30 20:37:01 -0800

    strict

--- src/start-block-stream.js
+++ src/start-block-stream.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var createBlockAndLogStreamer = require("./block-management/create-block-and-log-streamer");
 var createTransportAdapter = require("./block-management/ethrpc-transport-adapter");
 var onNewBlock = require("./block-management/on-new-block");
