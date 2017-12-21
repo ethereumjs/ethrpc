44360a2af74f2df93201f7c38e150c68fc3fe001
Ali Mousa <amousa11@gmail.com>
2017-12-21 10:12:34 -0800

    implement #32 suggestion
    This code was copied from @fanatid in https://github.com/ethereumjs/ethrpc/issues/32

--- src/validate/validate-address.js
+++ src/validate/validate-address.js
@@ -1,4 +1,21 @@
 "use strict";
+const sha3 = require("../utils/sha3");
+
+function checksumAddress (address) {
+  address = address.slice(2).toLowerCase();
+  var hash = sha3(address, "hex");
+  var ret = "0x";
+
+  for (var i = 0; i < address.length; i++) {
+    if (parseInt(hash[i], 16) >= 8) {
+      ret += address[i].toUpperCase();
+    } else {
+      ret += address[i];
+    }
+  }
+
+  return ret;
+}
 
 var validateAddress = function (address) {
   if (address === null || address === undefined) {
@@ -7,15 +24,17 @@ var validateAddress = function (address) {
   if (typeof address !== "string") {
     throw new Error("address must be a string but was " + typeof address);
   }
-  // fixup malformed addresses
-  if (/^[0-9a-fA-F]*$/.test(address)) {
+  if (address.slice(0, 2) !== "0x") {
     address = "0x" + address;
-  }
-  if (!/^0x[0-9a-fA-F]*$/.test(address)) {
-    throw new Error("address can only contain 0-9 and a-Z and must start with 0x.  Provided: " + address);
   }
   if (address.length !== 42) {
     throw new Error("address must be 42 characters, 20 bytes (2 hex encoded code points each) plus the 0x prefix.  Length: " + address.length);
+  }
+  if (!/^0x[0-9a-fA-F]{40}$/.test(address)) {
+    throw new Error("address can only contain 0-9 and a-F and must start with 0x.  Provided: " + address);
+  }
+  if (/[a-f]+/.test(address) && /[A-F]+/.test(address) && checksumAddress(address) !== address) {
+    throw new Error("address checksum is invalid.");
   }
   return address;
 };
