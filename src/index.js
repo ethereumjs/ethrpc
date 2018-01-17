843425a6040324bcdeb9529797ed68126ddd7672
Paul Gebheim <paul@onetaste.us>
2018-01-17 14:05:54 -0800

    Add pre-install/publish/version hooks and put keep version in ethrpc

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@
 var createEthrpc = require("./create-ethrpc");
 var reducer = require("./reducers");
 var composeReducers = require("./reducers/compose-reducers");
+var version = require("./version");
 
 var ethrpc = createEthrpc(reducer);
 ethrpc.withCustomReducer = function (customReducer) {
   return createEthrpc(composeReducers(customReducer, reducer));
 };
 
+ethrpc.version = version;
 module.exports = ethrpc;
