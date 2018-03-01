3ea8241b1afce92b29c370b4137f0f5a8082d208
Jack Peterson <jack@tinybike.net>
2018-02-28 19:55:18 -0800

    Removed sync path from ensureLatestBlock

--- src/block-management/ensure-latest-block.js
+++ src/block-management/ensure-latest-block.js
@@ -2,28 +2,19 @@
 
 var onNewBlock = require("../block-management/on-new-block");
 var eth_getBlockByNumber = require("../wrappers/eth").getBlockByNumber;
-var isFunction = require("../utils/is-function");
 
 /**
  * Ensures that we have the latest block.
  */
 function ensureLatestBlock(callback) {
   return function (dispatch) {
-    var block;
-    if (!isFunction(callback)) {
-      block = dispatch(eth_getBlockByNumber(["latest", false]));
-      if (block && !block.error && !(block instanceof Error)) {
+    dispatch(eth_getBlockByNumber(["latest", false], function (err, block) {
+      if (err) return callback(err);
+      if (block != null) {
         dispatch(onNewBlock(block));
-        return block;
+        callback(null, block);
       }
-    } else {
-      dispatch(eth_getBlockByNumber(["latest", false], function (block) {
-        if (block && !block.error) {
-          dispatch(onNewBlock(block));
-          callback(block);
-        }
-      }));
-    }
+    }));
   };
 }
 
