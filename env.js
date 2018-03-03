8b68e8d12d19da55355dc50311884b2343f34d68
Jack Peterson <jack@tinybike.net>
2018-03-02 21:03:50 -0800

    Added error checks to env.js

--- env.js
+++ env.js
@@ -13,9 +13,11 @@ rpc.setDebugOptions({ connect: true });
 rpc.connect(configuration, function (err) {
   if (err) return console.error("ethrpc connection failed:", err);
   rpc.eth.coinbase(function (err, coinbase) {
+    if (err) return console.error("eth_coinbase failed");
     global.COINBASE = coinbase;
   });
   rpc.net.version(function (err, networkID) {
+    if (err || networkID == null) return console.error("net_version failed");
     global.NETWORK_ID = networkID;
   });
 });
