7167aa014e00bedf53ac6727b5032007a80f8cdd
Jack Peterson <jack@tinybike.net>
2018-05-03 16:26:39 -0700

    Added isMetaMask function to utils

---
+++ src/utils/is-meta-mask.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function isMetaMask() {
+  if (typeof window === "undefined") return false;
+  if (!window) return false;
+  if (!window.web3) return false;
+  if (!window.web3.currentProvider) return false;
+  if (!window.web3.currentProvider.isMetaMask) return false;
+  return true;
+}
+
+module.exports = isMetaMask;
