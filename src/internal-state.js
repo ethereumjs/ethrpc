ec9698c76a4cefe668a9c61c4b1614b842fb819f
Scott Bigelow <epheph@gmail.com>
2018-05-16 13:24:47 -0700

    Be consistent within a single file

--- src/internal-state.js
+++ src/internal-state.js
@@ -1,12 +1,11 @@
 "use strict";
 
-var assign = require("lodash").assign;
 var lodash = require("lodash");
 
 var state = {};
 
 module.exports.getState = function () { return state; };
 module.exports.get = function (path) { return lodash.get(state, path); };
-module.exports.setState = function (newState) { assign(state, newState); };
+module.exports.setState = function (newState) { lodash.assign(state, newState); };
 module.exports.set = function (path, newState) { lodash.set(state, path, newState); };
 module.exports.unset = function (path) { lodash.unset(state, path); };
