29841ca365d695f3923590f396866b5c641c58b9
Justin Barry <>
2018-05-16 11:47:56 -0700

    Use full lodash package to prevent memory leak
    Current theory:
    
    When using separate 'lodash.set' and 'lodash.unset' two WeakMap memoize
    caches are created. Both will hold a references to common objects
    preventing garbage collection.

--- src/internal-state.js
+++ src/internal-state.js
@@ -1,14 +1,12 @@
 "use strict";
 
 var assign = require("lodash.assign");
-var get = require("lodash.get");
-var set = require("lodash.set");
-var unset = require("lodash.unset");
+var lodash = require("lodash");
 
 var state = {};
 
 module.exports.getState = function () { return state; };
-module.exports.get = function (path) { return get(state, path); };
+module.exports.get = function (path) { return lodash.get(state, path); };
 module.exports.setState = function (newState) { assign(state, newState); };
-module.exports.set = function (path, newState) { set(state, path, newState); };
-module.exports.unset = function (path) { unset(state, path); };
+module.exports.set = function (path, newState) { lodash.set(state, path, newState); };
+module.exports.unset = function (path) { lodash.unset(state, path); };
