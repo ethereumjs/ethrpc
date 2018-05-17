33a2ab2c02587d3f66d81e9b9da34ad3215b2ab9
Jack Peterson <jack@tinybike.net>
2018-05-17 16:29:27 -0700

    Set GAS_ESTIMATE_MULTIPLIER to 1.25 (seems to prevent tx failure)

--- src/constants.js
+++ src/constants.js
@@ -30,7 +30,7 @@ module.exports = {
   // how frequently to poll when waiting for blocks
   BLOCK_POLL_INTERVAL: 30000,
 
-  GAS_ESTIMATE_MULTIPLIER: new BigNumber("1.1", 10), // TODO adjust this empirically
+  GAS_ESTIMATE_MULTIPLIER: new BigNumber("1.25", 10), // TODO adjust this empirically
 
   DEFAULT_ETH_CALL_GAS: "0x5d1420",
 
