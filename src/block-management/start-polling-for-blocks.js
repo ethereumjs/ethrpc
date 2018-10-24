479ff819df428380bc348c16676ab89ae01889ac
Paul Gebheim <paul@onetaste.us>
2018-10-24 16:22:58 +0000

    Check for bad block data before going into the reducer

--- src/block-management/start-polling-for-blocks.js
+++ src/block-management/start-polling-for-blocks.js
@@ -2,6 +2,7 @@
 
 var eth_getBlockByNumber = require("../wrappers/eth").getBlockByNumber;
 var onNewBlock = require("./on-new-block");
+var validateBlock = require("../validate/validate-block");
 var observeCurrentBlockStateChanges = require("../store-observers/current-block");
 
 function startPollingForBlocks() {
@@ -13,6 +14,8 @@ function startPollingForBlocks() {
       if (getState().debug.broadcast) console.log("Polling for latest block...");
       dispatch(eth_getBlockByNumber(["latest", false], function (err, block) {
         if (err) return console.error(err);
+        if (block === null) return console.error(new RPCError("BLOCK_NOT_FOUND"));
+        if (!validateBlock(block)) return console.error(new RPCError("INVALID_BLOCK", { block: block }));
         dispatch({ type: "SET_CURRENT_BLOCK", data: block });
       }));
     }, getState().configuration.pollingIntervalMilliseconds);
