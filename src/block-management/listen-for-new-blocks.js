5124764b1672c4440c1d018245397f5c6c762afe
Paul Gebheim <pgebheim@gmail.com>
2020-01-03 01:12:45 +0000

    Revert "just poll instead of sometimes trying subscribe"
    This reverts commit e900fa6d3396dd38717b33e27ef6817434800647.

--- src/block-management/listen-for-new-blocks.js
+++ src/block-management/listen-for-new-blocks.js
@@ -1,10 +1,21 @@
 "use strict";
 
 var startPollingForBlocks = require("./start-polling-for-blocks");
+var subscribeToNewBlockNotifications = require("./subscribe-to-new-block-notifications");
+var unsubscribeFromNewBlockNotifications = require("./unsubscribe-from-new-block-notifications");
+var isMetaMask = require("../utils/is-meta-mask");
 
 function listenForNewBlocks() {
   return function (dispatch) {
-    dispatch(startPollingForBlocks());
+    if (isMetaMask()) { // MetaMask doesn't throw an error on eth_subscribe, but doesn't actually send block notifications, so we need to poll instead...
+      dispatch(startPollingForBlocks());
+    } else {
+      dispatch(subscribeToNewBlockNotifications(function (err) {
+        console.info("[ethrpc] eth_subscribe request failed, fall back to polling for blocks:", err.message);
+        dispatch(unsubscribeFromNewBlockNotifications());
+        dispatch(startPollingForBlocks());
+      }));
+    }
   };
 }
 
